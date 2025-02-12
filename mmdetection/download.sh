264708b5506a4fa965e70705809c4c44beed08d2
atomon <38009289+atomon@users.noreply.github.com>
2025-02-12 17:23:32 +0900

    fix: mainor correction

--- mmdetection/download.sh
+++ mmdetection/download.sh
@@ -6,9 +6,9 @@ wget http://images.cocodataset.org/zips/train2017.zip
 wget http://images.cocodataset.org/zips/val2017.zip
 wget http://images.cocodataset.org/zips/test2017.zip
 
-unzip train2017.zip
-unzip val2017.zip
-unzip test2017.zip
+unzip -p train2017.zip
+unzip -p val2017.zip
+unzip -p test2017.zip
 
 rm train2017.zip
 rm val2017.zip
@@ -16,5 +16,5 @@ rm test2017.zip
 
 cd ../
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-unzip annotations_trainval2017.zip
+unzip -p annotations_trainval2017.zip
 rm annotations_trainval2017.zip
